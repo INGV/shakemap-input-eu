325105d2a73073943e594587f54bf1de224b0951
Valentino Lauciani <valentino.lauciani@ingv.it>
2022-02-07 15:24:19 +0100

    Update code to user 'origin_push' remote to push new data

--- shakedata.py
+++ shakedata.py
@@ -370,7 +370,7 @@ def git_push():
     # repo.git.add('data')
     # logger.info(f"Executing commit")
     # repo.index.commit("Some XML data updated")
-    origin = repo.remote(name='origin')
+    origin = repo.remote(name='origin_push')
     logger.info(f"Executing push to {args.git_repo_dir}")
     origin.push()
 
